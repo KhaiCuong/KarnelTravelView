b1b3c5ff743c589db745f65877e09942094419c7
Yahu^^ <phamgiahung02@gmail.com>
2023-06-01 10:43:27 +0700

    ListAccommodation and CreateAccommodation

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import Home from "./components/Home/Home";
 import MainContent from "./components/MainContent/MainContent";
 import Footer from "./components/Footer/Footer";
 import TourManager from "./components/Admin/TourManager/TourManager";
+import Accommodation from "./components/Admin/Accommodation/Accommodation";
+import CreateAccommodation from "./components/Admin/Accommodation/CreateAccommodation";
+import ListAccommodation from "./components/Admin/Accommodation/ListAccommodation";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-  <>
-    <Header/>
-    {/* <Home/> */}
-    {/* <MainContent/> */}
-    <TourManager/>
-    <Footer/>
-  </>
+    <Routes >
+      <Route path="/accommodation/" element={<Accommodation />}>
+        <Route path="createAccommodation" element={<CreateAccommodation />} />
+        <Route path="listAccommodation" element={<ListAccommodation />} />
+      </Route>
+    </Routes>
+  //     {/* <Header/> */ }
+  // {/* <Home/> */ }
+  // {/* <MainContent/> */ }
+  // {/* <TourManager /> */ }
+  // {/* <Footer/> */ }
   );
 }
 
