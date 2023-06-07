89ed565793812a36cb07320497cff557a46fd29a
KhaiCuong <duongkhaicuong@gmail.com>
2023-06-07 18:26:08 +0700

    Cuong

--- src/components/Admin/Login/Service/ProtectRouter.js
+++ src/components/Admin/Login/Service/ProtectRouter.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
+
 function ProtectRouter({ children }) {
   const token = localStorage.getItem("token");
+  const usertoken = localStorage.getItem("userToken");
+
   if (token) {
-    return children;
+    if(JSON.parse(usertoken).role === "Admin" ){
+      return children;
+     }
+     else{
+      return <Navigate to="/" />;
+     }
+ 
   } else {
     return <Navigate to="/login" />;
   }
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import './NotFound.css'
+
+function NotFound() {
+    return ( 
+       
+        <img src="https://web4s.vn/uploads/tiny_uploads/tin-tuc/loi-404-not-found/loi-404-la-gi.jpg" className=' not-found' alt="" />
+
+     );
+}
+
+export default NotFound;
